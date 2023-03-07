4abbadc2b490a42871f1dd0ad11975ce29ad1fc5
ankurchaks <60315740+ankurchaks@users.noreply.github.com>
2023-03-07 15:06:12 +0530

    Add pre-populated pick list

--- streamlite_app.py
+++ streamlite_app.py
@@ -21,7 +21,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
 
 # Let's put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index))
+# streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index),['Avacado','Strawberries'])
 
 # Display the table on page
 streamlit.dataframe(my_fruit_list)
