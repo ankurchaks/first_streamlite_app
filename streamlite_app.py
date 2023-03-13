641f1610d446aaf6ec2a77287f30adbe304a2eee
ankurchaks <60315740+ankurchaks@users.noreply.github.com>
2023-03-13 18:29:20 +0530

    Commented Imports

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,7 +1,8 @@
 import streamlit
-import pandas
-import requests
+# import pandas
+# import requests
 import snowflake.connector
+from urllib.error import URLerror
 
 streamlit.title("My Mom's New Healthy Diner")
 
