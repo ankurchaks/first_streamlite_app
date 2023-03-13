e8816c6ae29faec03cc353c0f44dc7067a46fea3
ankurchaks <60315740+ankurchaks@users.noreply.github.com>
2023-03-13 18:03:29 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -66,3 +66,8 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains")
 streamlit.text(my_data_row)
+
+# Let's Change the Streamlit Components to Make Things Look a Little Nicer
+my_data_row = my_cur.fetchone()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_row)
