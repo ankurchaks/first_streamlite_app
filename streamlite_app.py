7ee5b9878e5fab31c4bd07ee6efc1b5017c1e8c0
ankurchaks <60315740+ankurchaks@users.noreply.github.com>
2023-03-19 12:19:22 +0530

    Add Text Entry box

--- streamlite_app.py
+++ streamlite_app.py
@@ -69,3 +69,7 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains")
 streamlit.dataframe(my_data_row)
+
+# Add Text Entry Box
+add_my_fruit=streamlit.text_input('What fruit would you like information about?')
+streamlit.write('Thanks for adding', add_my_fruit)
