9062f831e97b4776522deb35eb0efcc5341a3e5f
ankurchaks <60315740+ankurchaks@users.noreply.github.com>
2023-03-19 12:14:17 +0530

    Update code to fetch all fruit names

--- streamlite_app.py
+++ streamlite_app.py
@@ -63,3 +63,9 @@ streamlit.text(my_data_row)
 my_data_row = my_cur.fetchone()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
+
+# Oops! Let's Get All the Rows, Not Just One
+my_cur.execute("SELECT * from fruit_load_list")
+my_data_row = my_cur.fetchall()
+streamlit.text("The fruit load list contains")
+streamlit.text(my_data_row)
